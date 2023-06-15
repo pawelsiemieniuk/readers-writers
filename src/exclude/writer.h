3f27d08fa46ca0bdd68977c4edf6e0f9dbcfe699
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 20:16:57 +0200

    Update writer.h

--- src/exclude/writer.h
+++ src/exclude/writer.h
@@ -5,12 +5,12 @@ void* writer();
 /*
  * Funkcja reprezentująca wątek pisarza.
  * 
- * Wątek pisarza próbuje uzyskać dostęp do czytelni, redukując liczbę pisarzy w kolejce
+ * Wątek pisarza próbuje uzyskać dostęp do czytelni, redukując liczbę pisarzy oczekujących na wejście
  * i zwiększając liczbę pisarzy w czytelni. Następnie odczekuje określony czas pisania,
  * po czym opuszcza czytelnię, redukując liczbę pisarzy w czytelni.
  * Jeśli są jeszcze inni pisarze oczekujący, sygnalizuje dostęp kolejnemu pisarzowi.
  * W przeciwnym razie sygnalizuje czytelnikom możliwość dostępu. 
- * Po odczekaniu czasu odpoczynku, zwiększa liczbę pisarzy w kolejce i kontynuuje działanie w pętli.
+ * Po odczekaniu czasu odpoczynku, zwiększa liczbę pisarzy w oczekujących na wejście i kontynuuje działanie w pętli.
  */
 
 #endif
