ae55f453713b5d14f81b205695af82a82ea6cd21
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 21:33:47 +0200

    Update func.h

--- src/func.h
+++ src/func.h
@@ -2,9 +2,24 @@
 #define FUNC
 
 void printStatus();
+/*
+ * Deklaracja funkcji do wyświetlania aktualnego stanu kolejek
+ * Wyświetla aktualny stan kolejek czytelników i pisarzy.
+ */
 
 void signalHandler();
+/*
+ * Deklaracja funkcji do obsługi sygnału
+ * Funkcja jest wywoływana w momencie obsługi sygnału.
+ * Przeprowadza ona analizę czasu działania wątków i wyświetla
+ * statystyki dotyczące oczekiwania w kolejce dla czytelników i pisarzy.
+ */
 
 void logEntryTime(int id);
+/*
+ * Deklaracja funkcji do logowania czasu wejścia wątku do kolejki
+ * Funkcja jest wywoływana, gdy wątek wchodzi do kolejki.
+ * Zapisuje ona czas wejścia wątku do kolejki oraz aktualizuje najdłuższy czas oczekiwania w kolejce.
+ */
 
 #endif
