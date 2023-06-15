0d96ab86349b2daa43576e4925b6a1bf2ccf0c4a
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 21:39:32 +0200

    Update writer.h

--- src/wstarv/writer.h
+++ src/wstarv/writer.h
@@ -2,5 +2,14 @@
 #define WRITER_WSTARV
 
 void* writer();
+/*
+ * Funkcja reprezentująca wątek pisarza.
+ * 
+ * Jeśli w czytelni niema czytelników ani pisarzy oraz na wejście nie oczekują żadni czytelnicy
+ * Wątek pisarza próbuje uzyskać dostęp do czytelni, redukując liczbę pisarzy oczekujących na wejście
+ * i zwiększając liczbę pisarzy w czytelni. Następnie odczekuje określony czas pisania,
+ * po czym opuszcza czytelnię, redukując liczbę pisarzy w czytelni.
+ * Po odczekaniu czasu odpoczynku, zwiększa liczbę pisarzy w oczekujących na wejście i kontynuuje działanie w pętli.
+ */
 
 #endif
