e0322a6a8a6d53723fc7afdd0fc516ec9fb2d2ca
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 21:19:23 +0200

    Update reader.h

--- src/wstarv/reader.h
+++ src/wstarv/reader.h
@@ -2,5 +2,13 @@
 #define READER_WSTARV
 
 void* reader();
-
+/*
+ * Funkcja reprezentująca wątek czytelnika.
+ * 
+ * Jeśli w czytelni niema pisarzy
+ * Wątek czytelnika próbuje uzyskać dostęp do czytelni, redukując liczbę czytelników oczekujących na wejście
+ * i zwiększając liczbę czytelników w czytelni. Następnie odczekuje określony czas czytania,
+ * po czym opuszcza czytelnię, redukując liczbę czytelników w czytelni.
+ * Po odczekaniu czasu odpoczynku, zwiększa liczbę czytelników oczekujących na wejście i kontynuuje działanie w pętli.
+ */
 #endif
