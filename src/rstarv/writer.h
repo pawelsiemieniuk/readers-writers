1d63f4802d55f8cccfcf58b3bb85845368f99708
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 21:01:17 +0200

    Update writer.h

--- src/rstarv/writer.h
+++ src/rstarv/writer.h
@@ -2,5 +2,15 @@
 #define WRITER_RSTARV
 
 void* writer();
+/*
+ * Funkcja reprezentująca wątek pisarza.
+ * 
+ * Wątek pisarza próbuje uzyskać dostęp do czytelni, redukując liczbę pisarzy oczekujących na wejście
+ * i zwiększając liczbę pisarzy w czytelni. Następnie odczekuje określony czas pisania,
+ * po czym opuszcza czytelnię, redukując liczbę pisarzy w czytelni.
+ * Jeśli są jeszcze inni pisarze oczekujący, sygnalizuje dostęp kolejnemu pisarzowi.
+ * W przeciwnym razie sygnalizuje czytelnikom możliwość dostępu. 
+ * Po odczekaniu czasu odpoczynku, zwiększa liczbę pisarzy w oczekujących na wejście i kontynuuje działanie w pętli.
+ */
 
 #endif
