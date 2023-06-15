cbd3751d853157f150e07320c2ed5c00c4e5dc67
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 21:04:19 +0200

    Update writer.h

--- src/rstarv/writer.h
+++ src/rstarv/writer.h
@@ -8,8 +8,7 @@ void* writer();
  * Wątek pisarza próbuje uzyskać dostęp do czytelni, redukując liczbę pisarzy oczekujących na wejście
  * i zwiększając liczbę pisarzy w czytelni. Następnie odczekuje określony czas pisania,
  * po czym opuszcza czytelnię, redukując liczbę pisarzy w czytelni.
- * Jeśli są jeszcze inni pisarze oczekujący, sygnalizuje dostęp kolejnemu pisarzowi.
- * W przeciwnym razie sygnalizuje czytelnikom możliwość dostępu. 
+ * Jeśli niema już pisaży czekających na wejście, sygnalizuje czytelnikom możliwość dostępu.
  * Po odczekaniu czasu odpoczynku, zwiększa liczbę pisarzy w oczekujących na wejście i kontynuuje działanie w pętli.
  */
 
