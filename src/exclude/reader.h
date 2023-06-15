57f9d06d558082f30e0ff0fd74d1fba8bb6d2090
Fallen <135157962+Fallen-2001@users.noreply.github.com>
2023-06-15 19:58:56 +0200

    Update reader.h

--- src/exclude/reader.h
+++ src/exclude/reader.h
@@ -3,4 +3,15 @@
 
 void* reader();
 
+/*
+ * Funkcja reprezentująca wątek czytelnika.
+ * 
+ * Funkcja ta jest odpowiedzialna za logikę działania wątku czytelnika.
+ * Wątek czytelnika próbuje uzyskać dostęp do czytelni, redukując liczbę czytelników w kolejce
+ * i zwiększając liczbę czytelników w czytelni. Następnie odczekuje określony czas czytania,
+ * po czym opuszcza czytelnię, redukując liczbę czytelników w czytelni.
+ * Jeśli nie ma innych czytelników w czytelni, sygnalizuje pisarzom możliwość dostępu.
+ * Po odczekaniu czasu odpoczynku, zwiększa liczbę czytelników w kolejce i kontynuuje działanie w pętli.
+ */
+
 #endif
